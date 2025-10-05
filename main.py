963c7cda8a1fdb235ca4f4aedd0dd5aef33d2427
Dillon Chan <dillon@mosiman.ca>
2025-10-04 20:50:24 -0400

    Really poopy UI, no data hookup or backend

--- main.py
+++ main.py
@@ -1,6 +1,126 @@
-def main():
-    print("Hello from spaceapps-2025!")
+from textual.app import App, ComposeResult
+from textual.containers import HorizontalGroup, VerticalGroup, Container, Grid, VerticalScroll
+from textual.screen import Screen
+from textual.widgets import Footer, Header, Label, Button, Rule, TextArea, Input
+
+import pyfiglet
+
+GAME_NAME = "FARMING SIM NAME TBD 2025"
+
+
+class TitlePage(Screen):
+    def compose(self) -> ComposeResult:
+        title_fig = pyfiglet.figlet_format(GAME_NAME)
+        yield Label(title_fig, id="title_label")
+        yield HorizontalGroup(Button("Start", id="start_btn"), Button("Options", id="opt_btn"), id="titlepage_menu_btns_grp")
+
+    def on_button_pressed(self, event: Button.Pressed) -> None:
+        if event.button.id == "start_btn":
+            self.app.push_screen(GameScreen())
+
+class GameScreen(Screen):
+    def compose(self) -> ComposeResult:
+        yield Container (
+            Label(GAME_NAME, id="game_title"),
+            HorizontalGroup(
+                FarmPlotVisible(classes="farmplot"),
+                TaskListAP(classes="task_list"),
+                Journal(classes="journal_logs"),
+            ),
+            Label("Farm sim vroom vroom brrrrrrrrr", id="game_subtitle"),
+            Input(placeholder="/help for help"),
+            Button("Back to title", id="back_btn"),
+        )
+
+    def on_button_pressed(self, event: Button.Pressed) -> None:
+        if event.button.id == "back_btn":
+            self.app.pop_screen()
+
+class TaskListAP(Container):
+    def compose(self) -> ComposeResult:
+        taskGrid = Grid(
+            Label("T1", classes="task_id"),
+            Label("Investigate B3", classes="task_desc"),
+            Label("1 AP", classes="task_cost"),
+            Label("T2", classes="task_id"),
+            Label("Fertilize D4", classes="task_desc"),
+            Label("1 AP", classes="task_cost"),
+            Label("T3", classes="task_id"),
+            Label("Irrigate ALL 20mm", classes="task_desc"),
+            Label("2 AP", classes="task_cost"),
+            classes="task_grid_list"
+        )
+        yield Label("Tasks:", id="tasks_title")
+        yield taskGrid
+
+class CommandLine(Input):
+    def compose(self) -> ComposeResult:
+        yield Input(placeholder="/help for help")
+
+
+class JournalEntry(VerticalGroup):
+    def compose(self) -> ComposeResult:
+        yield Rule()
+        yield Label("2025-01-01")
+        yield Rule()
+        yield Label("Investigate B3: Found pests!")
+        yield Label("Fertilized D4")
+        yield Label("Irrigate ALL 20mm")
+
+class Journal(VerticalScroll):
+    def compose(self) -> ComposeResult:
+        yield JournalEntry()
+        yield JournalEntry()
+        yield JournalEntry()
+        yield JournalEntry()
+        
+class FarmPlotVisibleBad(Grid):
+    """ The farm plot is hardcoded to be 4x4 for the purposes of this hackathon """
+
+    def compose(self) -> ComposeResult:
+        yield Label("A1", id="farmplot_A1", classes="sector")
+        yield Label("A2", id="farmplot_A2", classes="sector")
+        yield Label("A3", id="farmplot_A3", classes="sector")
+        yield Label("A4", id="farmplot_A4", classes="sector")
+        yield Label("B1", id="farmplot_B1", classes="sector")
+        yield Label("B2", id="farmplot_B2", classes="sector")
+        yield Label("B3", id="farmplot_B3", classes="sector")
+        yield Label("B4", id="farmplot_B4", classes="sector")
+        yield Label("C1", id="farmplot_C1", classes="sector")
+        yield Label("C2", id="farmplot_C2", classes="sector")
+        yield Label("C3", id="farmplot_C3", classes="sector")
+        yield Label("C4", id="farmplot_C4", classes="sector")
+        yield Label("D1", id="farmplot_D1", classes="sector")
+        yield Label("D2", id="farmplot_D2", classes="sector")
+        yield Label("D3", id="farmplot_D3", classes="sector")
+        yield Label("D4", id="farmplot_D4", classes="sector")
+
+class FarmPlotVisible(Grid):
+    """ The farm plot is hardcoded to be 4x4 for the purposes of this hackathon """
+
+    def compose(self) -> ComposeResult:
+        alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        height = 4
+        width = 4
+
+        self.grid = [
+            [ Label(f"{row}{col}", id=f"farmplot_{row}{col}", classes="sector") for col in range(1,width+1)]
+            for row in alphabet[0:height]
+        ]
+        for r in range(0, height):
+            for c in range(0, width):
+                yield self.grid[r][c]
+
+class FarmingSimApp(App):
+    """ An interactive game for the 2025 NASA SpaceApps """
+
+    CSS_PATH = "fs.tcss"
+
+    def on_mount(self) -> None:
+        self.push_screen(TitlePage(id = "titlescreen"))
+
 
 
 if __name__ == "__main__":
-    main()
+    app = FarmingSimApp()
+    app.run()
